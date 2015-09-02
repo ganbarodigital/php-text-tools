9901757ae5eb4d54b162e9431787f7df7f472f30
Stuart Herbert <stuart@stuartherbert.com>
2015-09-02 08:38:00 +0100

    New filter: FilterOutMatchingRegex

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,9 +5,10 @@
 ### New
 
 * ExpandRange - now supports comma-separated ranges, and single numbers (e.g. "3, 6-8, 10")
-* Filters\FilterForMatchingRegex - simple regex `grep`-like utility class
-* Filters\FilterForMatchingString - simple non-regex `grep`-like utility class
-* Filters\FilterOutMatchingString - simple non-regex `grep -v`-like utility class
+* Filters\FilterForMatchingRegex - simple regex `grep` like utility class
+* Filters\FilterOutMatchingRegex - simple regex `grep -v` like utility class
+* Filters\FilterForMatchingString - simple non-regex `grep` like utility class
+* Filters\FilterOutMatchingString - simple non-regex `grep -v` like utility class
 
 ## 1.0.0 - Tue Sep 1 2015
 
---
+++ src/Filters/FilterOutMatchingRegex.php
@@ -0,0 +1,172 @@
+<?php
+
+/**
+ * Copyright (c) 2015-present Ganbaro Digital Ltd
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the names of the copyright holders nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  Libraries
+ * @package   TextTools/Filters
+ * @author    Stuart Herbert <stuherbert@ganbarodigital.com>
+ * @copyright 2015-present Ganbaro Digital Ltd www.ganbarodigital.com
+ * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link      http://code.ganbarodigital.com/php-text-tools
+ */
+
+namespace GanbaroDigital\TextTools\Filters;
+
+use GanbaroDigital\Reflection\Requirements\RequireStringy;
+use GanbaroDigital\Reflection\Requirements\RequireTraversable;
+use GanbaroDigital\Reflection\ValueBuilders\FirstMethodMatchingType;
+use GanbaroDigital\TextTools\Exceptions\E4xx_UnsupportedType;
+
+class FilterOutMatchingRegex
+{
+    /**
+     * return the input string if it doesn't match the search regex
+     *
+     * @param  string $data
+     *         the input string to check
+     * @param  string $searchRegex
+     *         the regex to match
+     * @return string|null
+     *         returns $data if it doesn't match the regex
+     *         returns NULL otherwise
+     */
+    public static function againstString($data, $searchRegex)
+    {
+        // robustness!
+        RequireStringy::checkMixed($data, E4xx_UnsupportedType::class);
+        RequireStringy::checkMixed($searchRegex, E4xx_UnsupportedType::class);
+
+        return self::matchLine($data, $searchRegex);
+    }
+
+    /**
+     * return the array values that do not match the search regex
+     *
+     * @param  array|Traversable $data
+     *         the collection of input strings to check
+     * @param  string $searchRegex
+     *         the regex to match
+     * @return array
+     *         returns the elements of $data that do not match the regex
+     *         will be an empty array if every value matches the regex
+     */
+    public static function againstTraversable($data, $searchRegex)
+    {
+        // robustness!
+        RequireTraversable::checkMixed($data, E4xx_UnsupportedType::class);
+        RequireStringy::checkMixed($searchRegex, E4xx_UnsupportedType::class);
+
+        return self::matchArray($data, $searchRegex);
+    }
+
+    /**
+     * return the parts of $data that do not match the regex
+     *
+     * @param  mixed $data
+     *         the collection of input strings to check
+     * @param  string $searchRegex
+     *         the regex to match
+     * @return mixed|null
+     *         returns the parts of $data that do not match the regex
+     *         returns NULL otherwise
+     */
+    public static function against($data, $searchRegex)
+    {
+        $method = FirstMethodMatchingType::fromMixed($data, self::class, 'against', E4xx_UnsupportedType::class);
+        return self::$method($data, $searchRegex);
+    }
+
+    /**
+     * return the parts of $data that do not match the regex
+     *
+     * @param  mixed $data
+     *         the collection of input strings to check
+     * @param  string $searchRegex
+     *         the regex to match
+     * @return mixed|null
+     *         returns the parts of $data that do not match the regex
+     *         returns NULL otherwise
+     */
+    public function __invoke($data, $searchRegex)
+    {
+        return self::against($data, $searchRegex);
+    }
+
+    /**
+     * search an array for values that do not match the regex
+     *
+     * @param  array|Traversable $data
+     *         the array to search
+     * @param  string $searchRegex
+     *         the regex to match
+     * @return array
+     *         only those values of $data that do not match the regex
+     */
+    private static function matchArray($data, $searchRegex)
+    {
+        $retval = [];
+
+        foreach ($data as $line) {
+            RequireStringy::checkMixed($line, E4xx_UnsupportedType::class);
+            $match = self::matchLine($line, $searchRegex);
+
+            if (!empty($match)) {
+                $retval[] = $match;
+            }
+        }
+
+        // all done
+        return $retval;
+    }
+
+    /**
+     * does the input string match the regex?
+     *
+     * @param  string $line
+     *         the input string to check
+     * @param  string $searchRegex
+     *         the regex to match
+     * @return string|null
+     *         returns $line if it does not match the regex
+     *         returns NULL otherwise
+     */
+    private static function matchLine($line, $searchRegex)
+    {
+        if (!preg_match($searchRegex, $line)) {
+            return $line;
+        }
+
+        return null;
+    }
+}
